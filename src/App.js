e62e3f5cb81e4c2ee59c930bb271283eb9ce2a51
Rami Leghrib <95674555+RamiLGH8@users.noreply.github.com>
2023-12-22 21:17:57 +0100

    routes are done!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import  {Home} from './components/home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navigationbar from './components/navbar/Navbar';
 import Details from './components/details/Details';
 import Registrations from './components/registrations/Registrations';
 function App() {
   return (
     <>
-     <Navigationbar/>
-     <Home />
-     <Details />
-     <Registrations />
+
+        <Navigationbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/register" element={<Registrations />} />
+       </Routes>
+
+  
    </> 
     );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,74 @@
 import Container from 'react-bootstrap/Container';
+import React,{useState} from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import '../navbar/navbar.css'
+import '../navbar/navbar.css';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import MenuIcon from '@mui/icons-material/Menu';
+
 function Navigationbar() {
+  const [show, setShow] = useState(false);
+  const [display,setDisplay]=useState('none');
+  
+  const handleClose = () =>{ 
+                   setShow(false);
+                   setDisplay('none');
+                  };
+  const handleShow = () =>{
+                   setShow(true);
+                   setDisplay('block');  
+                  }
 
     return (
-      
+     <> 
      <div className='d-none d-lg-block'>
       <Navbar style={{zIndex: "1", position:"fixed",top:"0",width:'100%',}}>
       <Container>
         <Navbar.Brand href='#home' ><img src='logo.png' alt='' style={{height:'51px',width:'310px'}}></img></Navbar.Brand>
         <Nav className="me-auto" style={{marginLeft:'35%'}}>
-          <Nav.Link href="#home" style={{color:'#FFFFFF' ,marginRight:'20px',fontSize:'20px'}}>Home</Nav.Link>
-          <Nav.Link href="#details" style={{color:'#FFFFFF' ,marginRight:'20px',fontSize:'20px'}}>Details</Nav.Link>
-          <Nav.Link href="#register" style={{color:'#FFFFFF' ,marginRight:'20px',fontSize:'20px'}}><button className='Register-Now'>Register</button></Nav.Link>
+                  <Nav.Link 
+                          href="/"   
+                          onMouseOver={(e) => e.target.style.borderBottom = '2px solid white'}  
+                          onMouseOut={(e) => e.target.style.borderBottom = '2px solid transparent'}
+                          style={{color:'#FFFFFF' ,marginRight:'20px',fontSize:'20px',}}>Home</Nav.Link>
+                  <Nav.Link 
+                          href="/details"
+                          onMouseOver={(e) => e.target.style.borderBottom = '2px solid white'}  
+                          onMouseOut={(e) => e.target.style.borderBottom = '2px solid transparent'}
+                          style={{color:'#FFFFFF' ,marginRight:'20px',fontSize:'20px'}}>Details</Nav.Link>
+                  <Nav.Link 
+                          href="/register"
+                          style={{color:'#FFFFFF' ,marginRight:'20px',fontSize:'20px'}}><button className='Register-Now'>Register</button></Nav.Link>
         </Nav>
+         {/* Mobile navbar */}
+    <div className='d-lg-none'>
+      <Nav style={{position:'fixed',top:'0', width:'100%',display:'flex',}}>
+        <Nav.Item style={{marginRight:'auto',height:'90px',display:'flex',alignItems:'center',marginLeft:'10px'}}>
+        <Navbar.Brand href='#home' style={{width:'100px'}} ><img src='logo.png' alt='' style={{height:'51px',width:'310px'}}></img></Navbar.Brand>
+        
+        </Nav.Item>
+        { <Nav.Item style={{display:'flex',alignItems:'center',marginRight:'10px'}}>
+        <btn onClick={handleShow}>
+          <MenuIcon style={{color:'#FFFFFF',fontSize:'50px',marginLeft:'auto',}}/>
+        </btn>
+        </Nav.Item> }
+{       
+      <Offcanvas style={{display:display}} show={show} onHide={handleClose} placement='end'>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title></Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body >
+          <Nav.Link href="/" style={{color:'#020617' ,marginRight:'20px',fontSize:'20px',marginLeft:'10px',marginBottom:'15px'}}>Home</Nav.Link>
+          <Nav.Link href="/details" style={{color:'#020617' ,marginRight:'20px',fontSize:'20px',marginLeft:'10px',marginBottom:'15px'}}>Details</Nav.Link>
+          <Nav.Link href="/register" style={{color:'#020617' ,marginRight:'20px',fontSize:'20px',marginLeft:'10px',marginBottom:'15px'}}>Register</Nav.Link>
+        </Offcanvas.Body>
+      </Offcanvas> }
+      </Nav>
+      </div>
       </Container>
     </Navbar>
     </div>
-   
+   </>
     );
   }
   
